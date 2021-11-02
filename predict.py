3294327a3b8e4ffd9b78d723834a2b0940fd84a7
Jaime Tan <tzmjaime@gmail.com>
2021-11-02 15:20:17 +0800

    Updated predict.py as flask app to return prediction when posted a json request

--- predict.py
+++ predict.py
@@ -25,48 +25,28 @@ dv_path = os.path.join(dirname, 'dv.bin')
 with open(dv_path, 'rb') as f_in:
     dv = pickle.load(f_in)
 
-test = {'age': 47,
-        'gender': "Male",
-        'polyuria': "No",
-        'polydipsia': "No",
-        'sudden weight loss': "No",
-        'weakness': "No",
-        'polyphagia': "No",
-        'genital thrush': "No",
-        'visual blurring': "No",
-        'itching': "Yes", 
-        'irritability': "No",
-        'delayed healing': "No",
-        'partial paresis': "No",
-        'muscle stiffness': "No",
-        'alopecia': "Yes",
-        'obesity': "No"
-        }
+app = Flask('churn')
 
-X = dv.transform([test])
-y_prob_lr = lr_model.predict_proba(X)[0, 1]
-y_prob_rfc = rfc_model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST'])
+def predict():
+    person = request.get_json()
+    X = dv.transform([person])
+    y_prob_lr = lr_model.predict_proba(X)[0, 1]
+    y_prob_rfc = rfc_model.predict_proba(X)[0, 1]
 
-features = dv.get_feature_names_out()
-dtest = xgb.DMatrix(data=X, feature_names=features)
-y_prob_xgbc = xgbc_model.predict(dtest)
+    features = dv.get_feature_names_out()
+    dtest = xgb.DMatrix(data=X, feature_names=features)
+    y_prob_xgbc = xgbc_model.predict(dtest)
 
-if len(y_prob_xgbc) == 1:
-    y_prob_xgbc = y_prob_xgbc[0]
+    if len(y_prob_xgbc) == 1:
+        y_prob_xgbc = y_prob_xgbc[0].item()
 
-print(f'Probability of having diabetes: \n' 
-      f'Logistic Regression Model:  {y_prob_lr} \n'
-      f'Random Forest Classifier:   {y_prob_rfc} \n' 
-      f'XGBoost Classifier:         {y_prob_xgbc} \n')
+    results = {
+        'diabetes_prob (Logistic Regression)': y_prob_lr,
+        'diabetes_prob (Random Forest Classifier)': y_prob_rfc,
+        'diabetes_prob (XGBoost Classifier)': y_prob_xgbc
+    }
+    return jsonify(results)
 
-# app = Flask('churn')
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     customer = request.get_json()
-#     X = dv.transform([customer])
-#     results = {'churn_proba': model.predict_proba(X)[0, 1]}
-#     return jsonify(results)
-
-# if __name__ == "__main__":
-#     app.run(debug=True, host='0.0.0.0', port=9696)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
