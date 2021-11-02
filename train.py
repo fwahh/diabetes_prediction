23e624130e8e7126f3f0b69f06cead2b9dbe3ba9
Jaime Tan <tzmjaime@gmail.com>
2021-11-02 14:35:00 +0800

    modified train.py to include dictvectorizer

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import pickle
 import pandas as pd
 import numpy as np
 
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb
@@ -22,24 +23,23 @@ if not os.path.exists('models'):
 
 data_path = os.path.join('data', 'diabetes.csv')
 df = pd.read_csv(data_path)
-df.columns = [var.lower() for var in df.columns]
+df.columns = [var.lower().replace(' ', '_') for var in df.columns]
 
 # We'll only be splitting into train and test set instead of train, validation and test set as cross validation will be used on the train set itself.
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=86431)
 df_train = df_train.reset_index(drop=True)
 df_test = df_test.reset_index(drop=True)
+y_train = (df_train['class'] == 'Positive').astype(int)
+y_test = (df_test['class'] == 'Positive').astype(int)
+del df_train['class']
+del df_test['class']
 
-X_train = df_train.applymap(lambda val: (0 if val in ('No', 'Negative', 'Male')
-                                         else 1 if val in ('Yes', 'Positive', 'Female')
-                                         else val))
-X_test = df_test.applymap(lambda val: (0 if val in ('No', 'Negative', 'Male')
-                                       else 1 if val in ('Yes', 'Positive', 'Female')
-                                       else val))
+# dict vectorizing values
+dv = DictVectorizer(sparse=False)
+X_train = dv.fit_transform(df_train.to_dict(orient='records'))
+X_test = dv.transform(df_test.to_dict(orient='records'))
 
-y_train = X_train['class'].values
-y_test = X_test['class'].values
-del X_train['class']
-del X_test['class']
+
 
 # Hyperparameters used below are already tuned. Refer to notebook for details on hyperparameter tuning.
 
@@ -87,8 +87,8 @@ with open(output_rfc, 'wb') as f_out:
 print(f'Random Forest Classifier model saved to: {output_rfc}\n\n')
 
 # XGBoost Classifier
-
-dtrain = xgb.DMatrix(data=X_train, label=y_train)
+features = dv.get_feature_names_out()
+dtrain = xgb.DMatrix(data=X_train, label=y_train, feature_names=features)
 xgb_params = {
     'eta': 0.3, 
     'max_depth': 6,
@@ -104,7 +104,7 @@ xgb_params = {
 xgbC = xgb.train(xgb_params, dtrain, num_boost_round=100)
 
 
-dtest = xgb.DMatrix(data=X_test, label=y_test)
+dtest = xgb.DMatrix(data=X_test, feature_names=features)
 y_pred_proba = xgbC.predict(dtest)
 y_pred = np.where(y_pred_proba >= 0.5, 1, 0)
 roc_auc_score(y_test, y_pred_proba)
@@ -120,4 +120,11 @@ output_xgbc = os.path.join('models', 'xgboost_classifier.bin')
 
 with open(output_xgbc, 'wb') as f_out:
     pickle.dump(xgbC, f_out)
-print(f'XGBoost Classifier model saved to: {output_xgbc}')
+print(f'XGBoost Classifier model saved to: {output_xgbc}\n\n')
+
+# Save dict vectorizer
+output_dv = os.path.join('models', 'dv.bin')
+
+with open(output_dv, 'wb') as f_out:
+    pickle.dump(dv, f_out)
+print(f'DictVectorizer saved to: {output_dv}')
